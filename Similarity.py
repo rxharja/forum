a46d0b8e631c4ffcee69864081fd98368521bf82
王雨晴 <ouuhare@wangyuqingdeMacBook-Pro.local>
2020-03-15 14:41:13 +0000

    data preprocessing

--- Similarity.py
+++ Similarity.py
@@ -4,6 +4,7 @@
 #  (based on catogory/family/mechanic)  #
 #                                       #
 #########################################
+#(requires data preprocessing to make sure there's no empty cell)
 
 # pip install python-Levenshtein
 import pandas as pd
@@ -28,17 +29,18 @@ df.set_index('game.id')
 
 def get_distance(game_name,factor,dataframe):
     similarity = []
-    category = dataframe.lookup(df[df['details.name'] == game_name].index,[factor])
+    category = dataframe.lookup(dataframe[dataframe['details.name'] == game_name].index,[factor])
     for index,row in dataframe.iterrows():
-        distance = Levenshtein.distance(','.join(category),str(row[factor]))
-        similarity.append(str(distance))
+        if str(row[factor]) != '':
+            distance = Levenshtein.distance(','.join(category),str(row[factor]))
+            similarity.append(str(distance))
     return similarity 
     
 
-def create_similarity_matrix(df,filename,game_name,factor1='attributes.boardgamefamily',factor2='attributes.boardgamecategory',factor3='attributes.boardgamemechanic'):
-    similarity1 = get_distance(game_name,factor1,df)
-    similarity2 = get_distance(game_name,factor2,df)
-    similarity3 = get_distance(game_name,factor3,df)
+def create_similarity_matrix(dataframe,filename,game_name,factor1='attributes.boardgamefamily',factor2='attributes.boardgamecategory',factor3='attributes.boardgamemechanic'):
+    similarity1 = get_distance(game_name,factor1,dataframe)
+    similarity2 = get_distance(game_name,factor2,dataframe)
+    similarity3 = get_distance(game_name,factor3,dataframe)
     mean_similarity = [game_name]
     for i in range(len(similarity1)):
         mean = int (similarity1[i]+similarity2[i]+similarity3[i]) / 3
@@ -48,12 +50,14 @@ def create_similarity_matrix(df,filename,game_name,factor1='attributes.boardgame
     file.write('\n')
     file.close()
     
-#create the matrix for the first 50 games
+#create the matrix for the first 20 games
+#test:
 #create_similarity_matrix(df,'game_similarity.csv','Die Macher')
     
 counter = 0
 for index2,row2 in df.iterrows():
     if counter <= 20:
+        #print(type(row2['details.name']))
         create_similarity_matrix(df,'game_similarity.csv',row2['details.name'])
         counter += 1
     
