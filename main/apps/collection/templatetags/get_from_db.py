097758abf4a4b701e3ce1b36bde95f0a03635837
s1977675 <s1977675@ed.ac.uk>
2020-03-30 22:05:26 +0100

    bugfix in db handler

--- main/apps/collection/templatetags/get_from_db.py
+++ main/apps/collection/templatetags/get_from_db.py
@@ -6,25 +6,21 @@ import pandas as pd
 
 
 def get_from_db(query, amount, to_pd=False):
-
-    default = DATABASES['default']['ENGINE']
-    print("working with the", default, "database")
-    if default == 'django.db.backends.postgresql_psycopg2':
-        try:
-            # web server heroku
-            con = psycopg2.connect(os.environ['DATABASE_URL'],
-                                   sslmode='require')
-        except KeyError:
-            con = psycopg2.connect(dbname=DATABASES['postgresql']['NAME'],
-                                   port='5432')
-    else:
-        try:
-            con = sqlite3.connect(DATABASES['mysql']['NAME'])
-        except RuntimeError:
-            return 'error!'
-    cursorObj = con.cursor()
-
     try:
+        default = DATABASES['default']['ENGINE']
+        print("working with the", default, "database")
+        if default == 'django.db.backends.postgresql_psycopg2':
+            try:
+                # web server heroku
+                con = psycopg2.connect(os.environ['DATABASE_URL'],
+                                       sslmode='require')
+            except KeyError:
+                con = psycopg2.connect(dbname=DATABASES['postgresql']['NAME'],
+                                       port='5432')
+        else:
+            con = sqlite3.connect(DATABASES['mysql']['NAME'])
+        cursorObj = con.cursor()
+
         if to_pd:
             return pd.read_sql_query(query, con)
         else:
@@ -35,8 +31,7 @@ def get_from_db(query, amount, to_pd=False):
                 return cursorObj.fetchone()
             else:
                 print("improper amount of queries to get chosen")
-    except RuntimeError:
-        print("Sorry! The query failed, check your syntax?")
-        return ''
+    except:
+        print("database error")
     finally:
         con.close()
